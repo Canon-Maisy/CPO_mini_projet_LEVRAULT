5efdf49f863b35626a7e28ac56347b20eadb2ead
Maxime <maxime.levrault@epfedu.fr>
2024-12-17 16:48:31 +0100

    nettoyage code VERSION JEU FONCTIONELLE CONSOLE

--- mini projet/src/mini/projet/MiniProjet.java
+++ mini projet/src/mini/projet/MiniProjet.java
@@ -13,29 +13,7 @@ import java.util.Scanner;
  */
 public class MiniProjet {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        //test classe GrilleDeJeu
-        /*GrilleDeJeu miaou = new GrilleDeJeu(5, 5);
-
-        Partie game = new Partie(miaou);
-        int[] diff = game.Difficulte();
-        game.initialiserPartie(diff[0], diff[0], diff[1]);
-        System.out.println(game);
-
-        boolean test = true;
-        while (test) {
-            game.tourDeJeu();
-            System.out.println(game);
-            test = !game.Victoire();
-            System.out.println("test : " + test);
-        }
-
-        System.out.println("fin de partie");*/
         
         Partie game = new Partie();
         game.demarrerPartie();
